6ab4da64592e196e0f6d64982b9b5932eb3ba71d
Benjamin Garcia <48642473+benjamingarcia10@users.noreply.github.com>
2020-09-24 00:24:50 -0700

    Added countdown text functionality

--- README.md
+++ README.md
@@ -4,6 +4,36 @@
 
 Time spent: **2** hours spent in total
 
+# Twitter - Part II
+
+This is a basic twitter app to view, compose, favorite, and retweet tweets.
+
+Time spent: **X** hours spent in total
+
+## User Stories
+
+The following **required** functionality is completed:
+
+- [X] User can compose a tweet. (4pts)
+- [X] User can favorite a tweet. (4pts)
+- [X] User can retweet a tweet. (2pts)
+
+The following **bonus** features are implemented:
+
+- [X] When composing a tweet, user sees a countdown for the number of characters remaining for the tweet (out of 280) (2pts)
+- [X] Tweet text field automatically expands to fit content.
+- [ ] User can view their profile in a profile tab. (3pts)
+- [ ] User sees embedded images in tweet if available. (3pts)
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented user stories:
+
+![App Walkthrough](/Media/Part-2.gif)
+
+
+# Twitter - Part I
+
 ## User Stories
 
 The following **required** functionality is completed:
@@ -18,7 +48,7 @@ The following **bonus** features are implemented:
 
 - [X] User can pull to refresh. (1pt)
 - [X] User can load past tweets infinitely. (2pts)
-- [X] Dark mode feature is added which statys persistent when app is restarted.
+- [X] Dark mode feature is added which stays persistent when app is restarted.
 
 ## Video Walkthrough
 
--- Twitter/TweetViewController.swift
+++ Twitter/TweetViewController.swift
@@ -8,14 +8,51 @@
 
 import UIKit
 
-class TweetViewController: UIViewController {
+class TweetViewController: UIViewController, UITextViewDelegate {
     
     @IBOutlet weak var tweetTextView: UITextView!
+    @IBOutlet weak var tweetLengthLabel: UILabel!
+    @IBOutlet weak var tweetContentHeightConstraint: NSLayoutConstraint!
+    // Set the max character limit
+    let characterLimit = 280
+    let minimumTextViewHeight = 150
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tweetTextView.becomeFirstResponder()
         // Do any additional setup after loading the view.
+        
+        tweetTextView.delegate = self
+        tweetTextView.layer.borderColor = UIColor.systemTeal.cgColor
+        tweetTextView.layer.borderWidth = 1.0
+        tweetTextView.layer.cornerRadius = 5.0
+        tweetContentHeightConstraint.constant = CGFloat(minimumTextViewHeight)
+        tweetLengthLabel.text = "Tweet Length: \(tweetTextView.text.count) / \(characterLimit)"
+         adjustTextViewHeight()
+    }
+    
+    func adjustTextViewHeight() {
+        let fixedWidth = tweetTextView.frame.size.width
+        let newSize = tweetTextView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
+        if (newSize.height > CGFloat(minimumTextViewHeight)) {
+            tweetContentHeightConstraint.constant = newSize.height
+            view.layoutIfNeeded()
+        }
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+         self.adjustTextViewHeight()
+        tweetLengthLabel.text = "Tweet Length: \(tweetTextView.text.count) / \(characterLimit)"
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        // TODO: Check the proposed new text character count
+        // Allow or disallow the new text
+        // Construct what the new text would be if we allowed the user's latest edit
+        let newText = NSString(string: textView.text!).replacingCharacters(in: range, with: text)
+
+        // The new text should be allowed? True/False
+        return newText.count <= characterLimit
     }
     
     @IBAction func cancel(_ sender: Any) {
